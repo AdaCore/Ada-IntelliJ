e21073990a3775e49242b4f30898a97ed3644e36
raja-s <rjsoufi@gmail.com>
2018-12-19 17:12:14 -0500

    Documented new Ada name validator

--- src/main/control/com/adacore/adaintellij/analysis/lexical/AdaNamesValidator.java
+++ src/main/control/com/adacore/adaintellij/analysis/lexical/AdaNamesValidator.java
@@ -4,8 +4,15 @@ import com.intellij.lang.refactoring.NamesValidator;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Name validator for Ada, used to perform basic syntactic
+ * checks on given names.
+ */
 public final class AdaNamesValidator implements NamesValidator {
 	
+	/**
+	 * @see com.intellij.lang.refactoring.NamesValidator#isKeyword(String, Project)
+	 */
 	@Override
 	public boolean isKeyword(@NotNull String name, Project project) {
 		
@@ -16,6 +23,9 @@ public final class AdaNamesValidator implements NamesValidator {
 		
 	}
 	
+	/**
+	 * @see com.intellij.lang.refactoring.NamesValidator#isIdentifier(String, Project)
+	 */
 	@Override
 	public boolean isIdentifier(@NotNull String name, Project project) {
 		
